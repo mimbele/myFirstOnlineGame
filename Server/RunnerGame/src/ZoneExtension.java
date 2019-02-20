cf38371a79cc1c8577595454bf03b1600c3b127d
friduby <friduby@gmail.com>
2019-02-20 13:43:00 +0330

    server project

---
+++ Server/RunnerGame/src/ZoneExtension.java
@@ -0,0 +1,11 @@
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+public class ZoneExtension extends  SFSExtension
+{
+    
+    @Override
+    public void init()
+    {
+//        addEventHandler(SFSEventType., LoginEventHandler.class);
+    }
+}
